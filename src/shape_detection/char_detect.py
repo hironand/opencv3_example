ccc117d70fce56ff2e490b7fb6109c81bcb7f5cb
hironand <dev.hironaga@gmail.com>
2016-12-10 21:09:57 +0900

    impl >> Brute-Force Matching >>  worked

--- src/shape_detection/char_detect.py
+++ src/shape_detection/char_detect.py
@@ -2,6 +2,7 @@
 __author__ = 'hironand'
 
 import cv2
+import numpy as np
 
 '''
 
@@ -21,28 +22,75 @@ def main():
     ei002 = cv2.imread("./../../image/ei002.png")
 
     # 特徴量抽出
-    kp_m, des_m = detect(ei_gothic, "ei_gothic.png")
-    kp_s, des_s = detect(ei001, "ei001.png")
+    kp_m, des_m = detect_AKAZE(ei_gothic, "ei_gothic.png")
+    kp_s, des_s = detect_AKAZE(ei001, "ei001.png")
 
     # マッチング
     master = "ei_gothic", ei_gothic, kp_m, des_m
     sample = "ei001", ei001, kp_s, des_s
-    match_FLANN(master, sample)
+    match_BF(master, sample)
 
 
-# 特徴量(KeyPoint, Descriptor)抽出
-def detect(img, name):
+# AKAZEで特徴量(KeyPoint, Descriptor)抽出
+def detect_AKAZE(img, name):
 
     detector = cv2.AKAZE_create()
     kp, des = detector.detectAndCompute(img, None)
     print("{}: keypoints: {}, descriptors: {}".format(name, len(kp), des.shape))
 
     cv2.drawKeypoints(img, kp, img, (0, 255, 0))
-    cv2.imwrite("./../../image/detect_" + name, img)
+    cv2.imwrite("./../../image/AKAZE_" + name, img)
 
     return kp, des
 
-# FLANN based Matcher で特徴量を比較
+
+# BF(Brute-Force) で特徴量を比較
+# * FLANNはPython2.7 では動作しなかった
+def match_BF((name_m, img_m, kp_m, des_m), (name_s, img_s, kp_s, des_s)):
+
+    # create BFMatcher object
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+    # Match descriptors.
+    matches = bf.match(des_m, des_s)
+
+    # Sort them in the order of their distance.
+    matches = sorted(matches, key = lambda x:x.distance)
+
+    # Draw first 10 matches.
+    out_img = cv2.drawMatches(img_m, kp_m, img_s, kp_s, matches ,None, flags=2)
+    out_name = "./../../image/BF_{}_{}.png".format(name_m, name_s)
+    cv2.imwrite(out_name, out_img)
+
+
+# BF(Brute-Force) で特徴量を比較
+# * FLANNはPython2.7 では動作しなかった
+def match_BF_((name_m, img_m, kp_m, des_m), (name_s, img_s, kp_s, des_s)):
+
+    # BFMatcher with default params
+    bf = cv2.BFMatcher()
+    matches = bf.knnMatch(des_m, des_s, k=2)
+
+    # Apply ratio test
+    good = []
+    for m,n in matches:
+        if m.distance < 0.75*n.distance:
+            good.append([m])
+
+    # Need to draw only good matches, so create a mask
+    matchesMask = [[0,0] for i in xrange(len(matches))]
+    draw_params = dict(matchColor = (0,255,0),
+                       singlePointColor = (255,0,0),
+                       matchesMask = matchesMask,
+                       flags = 0)
+
+    # cv2.drawMatchesKnn expects list of lists as matches.
+    out_img = cv2.drawMatchesKnn(img_m, kp_m, img_s, kp_s, good, outImg=np.zeros)
+    cv2.imwrite("./../../image/BF_{}_{}".format(name_m, name_s), out_img)
+
+
+
+# FLANN で特徴量を比較
 def match_FLANN((name_m, img_m, kp_m, des_m), (name_s, img_s, kp_s, des_s)):
     # FLANN parameters
     FLANN_INDEX_KDTREE = 0
@@ -65,8 +113,8 @@ def match_FLANN((name_m, img_m, kp_m, des_m), (name_s, img_s, kp_s, des_s)):
                        matchesMask = matchesMask,
                        flags = 0)
 
-    match_img = cv2.drawMatchesKnn(img_m, kp_m, img_s, kp_s, matches, None, **draw_params)
-    cv2.imwrite("./../../image/match_{}_{}".format(name_m, name_s), match_img)
+    out_img = cv2.drawMatchesKnn(img_m, kp_m, img_s, kp_s, matches, None, **draw_params)
+    cv2.imwrite("./../../image/FLANN_{}_{}".format(name_m, name_s), out_img)
 
 
 main()
